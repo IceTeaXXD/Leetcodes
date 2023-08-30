d103df570acd6eb6289eacafe82e829d81944ad3
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-08-30 21:50:15 +0700

    Time: 2095 ms (14.84%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT product_id, 
 ROUND(
-  SUM(price*units)/sum(units)
-  ,2) 
+  SUM(price*units)/SUM(units)
+,2) 
 AS average_price
 FROM unitssold NATURAL JOIN prices
 WHERE purchase_date BETWEEN start_date AND end_date
